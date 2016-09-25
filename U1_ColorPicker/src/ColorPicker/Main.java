79eda9a1e1a1054641cc9120317c83d4a0e95fc0
Matthias Langhard <matthias.langhard@ecologic.ch>
2016-09-25 13:24:14 +0200

    adds Menu

--- U1_ColorPicker/src/ColorPicker/Main.java
+++ U1_ColorPicker/src/ColorPicker/Main.java
@@ -2,6 +2,7 @@ package ColorPicker;
 
 import ColorPicker.Buttons.Buttons;
 import ColorPicker.ColorBox.ColorBox;
+import ColorPicker.Menu.Menu;
 import ColorPicker.RadioButtons.RadioButtons;
 import ColorPicker.Sliders.Sliders;
 import javafx.application.Application;
@@ -13,7 +14,15 @@ public class Main extends Application {
 
     @Override
     public void start(Stage mainStage) {
+        //Constants
+        int WIDTH = 300;
+        int HEIGHT = 320;
+
+        //ColorModel
         ColorModel colorModel = new ColorModel();
+
+        //UI Components
+        Menu menu = new Menu(colorModel);
         Sliders sliders = new Sliders(colorModel);
         ColorBox colorBox = new ColorBox(colorModel);
         RadioButtons radioButtons = new RadioButtons(colorModel);
@@ -21,17 +30,17 @@ public class Main extends Application {
 
         //Whole Pane
         FlowPane whole = new FlowPane();
-        whole.getChildren().addAll(sliders, colorBox, radioButtons, buttons);
+        whole.getChildren().addAll(menu, sliders, colorBox, radioButtons, buttons);
 
         mainStage.setScene(new Scene(whole));
         mainStage.setTitle("DEPA ColorPicker");
-        mainStage.setWidth(300);
-        mainStage.setHeight(300);
+        mainStage.setWidth(WIDTH);
+        mainStage.setHeight(HEIGHT);
         //Prevents resizing
-        mainStage.setMaxWidth(300);
-        mainStage.setMaxHeight(300);
-        mainStage.setMinWidth(300);
-        mainStage.setMinHeight(300);
+        mainStage.setMaxWidth(WIDTH);
+        mainStage.setMaxHeight(HEIGHT);
+        mainStage.setMinWidth(WIDTH);
+        mainStage.setMinHeight(HEIGHT);
 
         mainStage.show();
 
--- U1_ColorPicker/src/ColorPicker/Menu/Menu.java
+++ U1_ColorPicker/src/ColorPicker/Menu/Menu.java
@@ -1,46 +1,51 @@
-package ColorPicker.RadioButtons;
+package ColorPicker.Menu;
 
 import ColorPicker.ColorModel;
 import ColorPicker.Observer;
-
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.RadioButton;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.RadioMenuItem;
 import javafx.scene.control.Toggle;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
 
-public class RadioButtons extends VBox implements Observer {
+public class Menu extends VBox implements Observer {
     ColorModel colorModel;
 
-    ToggleGroup group = new ToggleGroup();
     @FXML
-    private RadioButton red;
+    private MenuItem exit;
     @FXML
-    private RadioButton blue;
+    private RadioMenuItem red;
     @FXML
-    private RadioButton green;
+    private RadioMenuItem blue;
     @FXML
-    private RadioButton yellow;
+    private RadioMenuItem green;
     @FXML
-    private RadioButton cyan;
+    private RadioMenuItem yellow;
     @FXML
-    private RadioButton orange;
+    private RadioMenuItem cyan;
     @FXML
-    private RadioButton black;
+    private RadioMenuItem orange;
+    @FXML
+    private RadioMenuItem black;
 
     boolean preventUpdate;
 
+    ToggleGroup group = new ToggleGroup();
 
-    public RadioButtons(final ColorModel colorModel) {
+
+    public Menu(ColorModel colorModel) {
         this.colorModel = colorModel;
         colorModel.registerObserver(this);
 
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("radioButtons.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("menu.fxml"));
         fxmlLoader.setRoot(this);
         fxmlLoader.setController(this);
         try {
@@ -48,6 +53,12 @@ public class RadioButtons extends VBox implements Observer {
         } catch (IOException exception) {
             throw new RuntimeException(exception);
         }
+
+        exit.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent t) {
+                System.exit(0);
+            }
+        });
 
         //Adding Radios to group
         red.setToggleGroup(group);
@@ -64,47 +75,47 @@ public class RadioButtons extends VBox implements Observer {
                 if (group.getSelectedToggle() != null && !preventUpdate) {
                     String selectedColor = group.getSelectedToggle().getUserData().toString();
                     colorModel.setColorByName(selectedColor);
-
                 }
-
             }
         });
 
+
     }
+
 
     @Override
     public void update(int r, int g, int b) {
-        if (r == 255 && g == 0 && b == 0) {
+        if (r == 255 && g == 0 && b == 0){
             preventUpdate = true;
             red.setSelected(true);
             preventUpdate = false;
         }
-        if (r == 0 && g == 0 && b == 255) {
+        if (r == 0 && g == 0 && b == 255){
             preventUpdate = true;
             blue.setSelected(true);
             preventUpdate = false;
         }
-        if (r == 0 && g == 255 && b == 0) {
+        if (r == 0 && g == 255 && b == 0){
             preventUpdate = true;
             green.setSelected(true);
             preventUpdate = false;
         }
-        if (r == 255 && g == 255 && b == 0) {
+        if (r == 255 && g == 255 && b == 0){
             preventUpdate = true;
             yellow.setSelected(true);
             preventUpdate = false;
         }
-        if (r == 0 && g == 255 && b == 255) {
+        if (r == 0 && g == 255 && b == 255){
             preventUpdate = true;
             cyan.setSelected(true);
             preventUpdate = false;
         }
-        if (r == 255 && g == 200 && b == 0) {
+        if (r == 255 && g == 200 && b == 0){
             preventUpdate = true;
             orange.setSelected(true);
             preventUpdate = false;
         }
-        if (r == 0 && g == 0 && b == 0) {
+        if (r == 0 && g == 0 && b == 0){
             preventUpdate = true;
             black.setSelected(true);
             preventUpdate = false;
